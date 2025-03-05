4e28ba65a52908074a95aab9a1a60c4c11f42baa
Yad <Yad@gmail.com>
2025-03-04 21:27:30 -0500

    String Type

--- src/Mar4/MultiDimensionArray.java
+++ src/Mar4/MultiDimensionArray.java
@@ -1,5 +1,7 @@
 package Mar4;
 
+import java.util.Scanner;
+
 public class MultiDimensionArray {
     public static void main(String[] args) {
         /*int [] []arr = {{10,20,30}, {40,50,60}};
@@ -9,7 +11,7 @@ public class MultiDimensionArray {
                 System.out.println("Elements of array are : " + elements  );
         }*/
 
-        int[][] arr = new int[2][3];
+        /*int[][] arr = new int[2][3];
         arr[0][0]=10;
         arr[0][1]=20;
         arr[0][2]=30;
@@ -22,6 +24,24 @@ public class MultiDimensionArray {
             for (int elements : num) {
                 System.out.println("Elements are :" + elements);
 
+            }
+        }*/
+        int [][] arr = new int[2][3];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Elements of Array :");
+        for (int i=0; i<2; i++)
+        {
+            for (int j=0; j<3 ;j++)
+            {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Elements of Array are : ");
+        for (int i=0; i<2; i++)
+        {
+            for (int j=0; j<3 ;j++)
+            {
+                System.out.println(arr[i][j]);
             }
         }
 
--- src/Mar4/readme.md
+++ src/Mar4/readme.md
@@ -2,3 +2,19 @@
 --Types of Arrays
 1. Single dimension
 2. Multi Dimension array
+
+
+## Strings
+1. Character array
+2. String is a class
+3. Non Primitive data type
+
+1. Using String Literal -- use the same memory called SCP (String constant pool) if string object is same.
+String are immutable, so when we try to assign different value or change exiting value,then it create new reference in memory
+and delete old one by garbage collector.
+
+String name = "Pragra"
+String company = "Pragra"
+String orgnization = "Pragra"
+
+2. Using new keyword -- create new memory outside SCP somewhere in heap, everytime object is created.
