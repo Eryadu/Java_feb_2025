a9db025491810c2e57af4df002160fea21934dad
Yad <Yad@gmail.com>
2025-03-09 20:52:46 -0400

    Instanceof Inheritance

---
+++ src/Mar8_Inheritance/Inheritance/ConstructorInvoke/SuperConstructorMain.java
@@ -0,0 +1,8 @@
+package Mar8_Inheritance.Inheritance.ConstructorInvoke;
+
+public class SuperConstructorMain {
+    public static void main(String[] args) {
+        SuperConstructor sc = new SuperConstructor(12);
+        sc.getNumber();
+    }
+}
