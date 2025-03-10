ed9c64900dbe3a832ebf035d694331a6bcec54d2
Yad <Yad@gmail.com>
2025-03-10 19:25:19 -0400

    Array Assignment Sum of Elements

---
+++ src/Assignment/Array/SumOfElements.java
@@ -0,0 +1,18 @@
+package Assignment.Array;
+
+import java.util.Scanner;
+
+public class SumOfElements {
+    public static void main(String[] args) {
+        int sum = 0;
+        int[] arr = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the elements of Array : ");
+        for (int i = 0 ; i <5 ; i++)
+        {
+            arr[i] = scanner.nextInt();
+            sum += arr[i];
+        }
+        System.out.println("Sum of Array Elements are : " + sum);
+    }
+}
