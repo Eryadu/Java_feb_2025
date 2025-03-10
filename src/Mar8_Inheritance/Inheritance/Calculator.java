db11f87272d51fe50cea75573cf2afe10771a066
Yad <Yad@gmail.com>
2025-03-09 20:11:14 -0400

    Super Keyword inheritance

---
+++ src/Mar8_Inheritance/Inheritance/Calculator.java
@@ -0,0 +1,11 @@
+package Mar8_Inheritance.Inheritance;
+
+public class Calculator {
+    int x =50 , y =10;
+    public void calculation()
+    {
+        System.out.println("This is a method of Calculator class.");
+    }
+
+
+}
---
+++ src/Mar8_Inheritance/Inheritance/MainSuperKeyword.java
@@ -0,0 +1,10 @@
+package Mar8_Inheritance.Inheritance;
+
+public class MainSuperKeyword {
+    public static void main(String[] args) {
+
+        FunctionOfCalculator functionOfCalculator = new FunctionOfCalculator();
+        functionOfCalculator.super_class();
+
+    }
+}
