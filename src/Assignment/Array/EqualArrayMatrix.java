207269df94b5f815518340a911ffef5187bcb5c9
Yad <Yad@gmail.com>
2025-03-10 21:12:08 -0400

    Sorted Array

--- src/Assignment/Array/EqualArrayMatrix.java
+++ src/Assignment/Array/EqualArrayMatrix.java
@@ -1,4 +1,45 @@
 package Assignment.Array;
 
 public class EqualArrayMatrix {
+    public static void main(String[] args) {
+        //Input matrix m1
+        int m1[][] = {
+
+                {6, 14, 16},
+                {4, 15, 17}
+        };
+
+        //Input matrix m2
+        int m2[][] = {
+                {19, 8, 17},
+                {16, 14, 16},
+                {4, 15, 17}
+        };
+
+        int rows1 = m1.length;
+        int col1 = m1[0].length;
+        int rows2 = m2.length;
+        int col2 = m2[0].length;
+        boolean flag = true;
+
+        if (rows1 != rows2 || col1 != col2) {
+            System.out.println("Given matrices are not equal");
+        } else {
+            for (int i = 0; i < rows1; i++) {
+                for (int j = 0; j < col1; j++) {
+                    if (m1[i][j] != m2[i][j]) {
+                        flag = false;
+                        break;
+                    }
+
+                }
+            }
+
+            if (flag) {
+                System.out.println("Matrices are equal.");
+            } else {
+                System.out.println("Matrices are not equal.");
+            }
+        }
+    }
 }
--- src/Assignment/Array/SortedArray.java
+++ src/Assignment/Array/SortedArray.java
@@ -2,23 +2,29 @@ package Assignment.Array;
 
 public class SortedArray {
     public static void main(String[] args) {
-        int [] arr = {10,2,21,22,5};
-        int secondMax = 0, firstMax = 0;
+        int [] arr = {10,2,21,22,5,32,1,45,6,345,0,8,54};
+        System.out.print("Array Elements are : ");
+        for(int nu : arr)
+        {
+            System.out.print( nu + " ");
+        }
+        System.out.println();
         for (int i = 0 ; i < arr.length ; i++)
         {
-            for (int j = 1 ; j < arr.length; j++)
+            for (int j = i+1 ; j < arr.length; j++ )
             {
-                if(arr[i]> arr[j])
+                if(arr[i] > arr[j])
                 {
                     int temp = arr[i];
                     arr[i] = arr [j];
                     arr[j] = temp;
-
                 }
             }
         }
-        for( int num : arr)
-        System.out.println(num);
+        System.out.print("Sorted Array Elements are : ");
+        for( int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 
 }
